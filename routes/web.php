ce83e397868e39fa6381465d9b960f5fd19c3a6d
Charles Haris <bbkch@Charless-MacBook-Pro.local>
2022-10-02 01:06:29 +0000

    first commit, first versin

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return view('/student');
+});
+Route::post("/student/store", [StudentController::class,'store'])->name('store');
+Route::get("/student", [StudentController::class,'index'])->name('index');
+Route::get("/student/create", [StudentController::class, 'create']);
+Route::get("/student/{id}", [StudentController::class, 'show']);
+Route::get("/student/{id}/edit", [StudentController::class, 'edit']);
+Route::post("/student/update/{id}", [StudentController::class,'update' ]);
+
+
+
+
+
