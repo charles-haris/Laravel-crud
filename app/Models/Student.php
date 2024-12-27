613de4336ac9807f5450a1ccb80aa0fa6057a8a4
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2024-12-27 17:20:08 +0000

    Update Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -10,6 +10,8 @@ class Student extends Model
     //protected $table = 'students';//table
     // why is this one commented
     // why is this one commented
+        // why is this one commented
+
 
     //protected $primaryKey = 'id';//cle primaire
     //protected $fillable = ['name', 'address', 'mobile'];//other champs
