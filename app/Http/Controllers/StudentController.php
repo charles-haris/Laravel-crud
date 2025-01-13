c1d0c0e1ce776cbbefdbc6f0cd5eda419d95d038
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2025-01-13 10:04:34 +0000

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,7 +7,7 @@ use App\Models\Student;
 
 class StudentController extends Controller
 {
-//this method is called index method 
+//this method is called index method, this method grants to get all the students using the Model Student 
     public function index()
     {
         $students = Student::all();
